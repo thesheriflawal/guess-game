463eee16278979f84426c31022e787878f9fd2aa
Sherif Lawal <lawalsherif0000@gmail.com>
2025-05-09 11:57:03 +0100

    difficulty update

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   const [hasGuessed, setHasGuessed] = useState(false);
   const [feedback, setFeedback] = useState("Feedback");
   const [remainingGuesses, setRemainingGuesses] = useState(5);
+  const [difficulty, setDifficulty] = useState("medium");
 
   const handleGuess = () => {
     if (userGuess === "" || hasGuessed || remainingGuesses <= 0) return;
@@ -17,7 +18,9 @@ function App() {
     setComputerGuess(randomNumber);
     const userNumber = parseInt(userGuess);
 
-    if (userNumber < randomNumber) {
+    if (userNumber > 100 || userNumber < 1 || isNaN(userNumber)) {
+      setFeedback("Invalid!");
+    } else if (userNumber < randomNumber) {
       setFeedback("Too low!");
     } else if (userNumber > randomNumber) {
       setFeedback("Too high!");
@@ -29,11 +32,15 @@ function App() {
     setRemainingGuesses((prev) => prev - 1);
   };
 
-  const handleRestart = () => {
+  const handleRestart = (level = difficulty) => {
     setComputerGuess(0);
     setUserGuess("");
     setHasGuessed(false);
     setFeedback("Feedback");
+
+    if (level === "easy") setRemainingGuesses(10);
+    else if (level === "medium") setRemainingGuesses(5);
+    else if (level === "hard") setRemainingGuesses(3);
   };
 
   return (
@@ -49,7 +56,14 @@ function App() {
           <h1>Number Guesser Game</h1>
           <div className="difficulty">
             <p>Difficulty: </p>
-            <select>
+            <select
+              value={difficulty}
+              onChange={(e) => {
+                const level = e.target.value;
+                setDifficulty(level);
+                handleRestart(level);
+              }}
+            >
               <option value="easy">Easy</option>
               <option value="medium">Medium</option>
               <option value="hard">Hard</option>
